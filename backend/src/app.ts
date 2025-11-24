99eee071cb650a2676f7f623535ae79d8e149655
Rajeshan Maharjan <rajeshanmaharjan.25@gmail.com>
2025-11-24 20:37:54 +0545

    first commit

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    credentials:true,
+    exposeHeaders: ['Access-Control-Allow-Credentials'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
+
+export default app;
